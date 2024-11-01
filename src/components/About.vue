c8eb37268eafb2c2ed161dfd85266bbfdaa4eab4
Sebastian Perez <perez.johngabriel13@gmail.com>
2024-11-01 22:16:04 +0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="About">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data() {
+    return {
+      title: 'About',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Gallery">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+      title: 'Gallery',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
